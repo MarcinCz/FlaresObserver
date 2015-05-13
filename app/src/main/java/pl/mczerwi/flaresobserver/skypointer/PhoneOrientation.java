969ea40a6ab734bb1fb4120fcf03ecc4eb565406
marcincz <marcinczrw@gmail.com>
2015-05-13 22:35:02 +0200

    ikonka aplikacji i daty

--- app/src/main/java/pl/mczerwi/flaresobserver/skypointer/PhoneOrientation.java
+++ app/src/main/java/pl/mczerwi/flaresobserver/skypointer/PhoneOrientation.java
@@ -8,16 +8,9 @@ class PhoneOrientation {
     private final double altitude;
     private final double azimuth;
 
-    public double getZAngle() {
-        return zAngle;
-    }
-
-    private final double zAngle;
-
     public PhoneOrientation(float[] orientation) {
         this.altitude = getAdjustedAltitude(orientation);
         this.azimuth = getAdjustedAzimuth(orientation);
-        this.zAngle = Math.toDegrees(orientation[2]);
     }
 
     private double getAdjustedAzimuth(float[] orientation) {
